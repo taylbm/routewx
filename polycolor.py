54e0ac1897b8e78b55a8259d67316e66ed540683
Brandon M. Taylor <Brandon.M.Taylor@noaa.gov>
2019-01-18 17:27:51 -0600

    beta test ready -bt

--- polycolor.py
+++ polycolor.py
@@ -28,16 +28,6 @@ class UTC(tzinfo):
     def dst(self, dt):
         return timedelta(0)
 
-tz = UTC()
-departure_time = datetime(2019, 1, 17, 13, 30, 31, tzinfo=tz)
-if 0 < departure_time.hour < 6:
-    model_init_date = departure_time - timedelta(days=1)
-else:
-    model_init_date = departure_time
-
-model_init_hour = 0 if 6 < departure_time.hour < 18 else 12
-model_init_date = model_init_date.replace(hour=model_init_hour, minute=0, second=0)
-
 def build_hrrr_request_uri():
     # Placeholder for now
     return None
@@ -60,18 +50,19 @@ def nearest_gridpoint(lats, lons, point_lat, point_lon, data):
     latlon_idx = np.argmin(c)
     gridpoint_temp = data[0].flat[latlon_idx]
     gridpoint_precip = data[1].flat[latlon_idx]
-    return (gridpoint_temp, gridpoint_precip)
+    gridpoint_frozen_precip = data[2].flat[latlon_idx]
+    return (gridpoint_temp, gridpoint_precip, gridpoint_frozen_precip)
 
 def weather_check(temp, precip):
     temp = temp - 273
     precip = precip * 0.039
     hazard_level = 'green'
-    if precip > 0.01:
+    if precip > 0.02:
         if temp <= 0 or precip > 10:
             hazard_level = 'red'
         else:
             hazard_level = 'yellow'
-    return hazard_level
+    return (hazard_level, temp, precip)
         
     
 #app.run()
@@ -81,13 +72,20 @@ def pline():
     decoded_polyline = polyline.decode(encoded_polyline)
     polyline_arr = np.asarray(decoded_polyline)
     duration = int(request.args.get('duration')) / 3600
-    print duration
+    departure_time = int(request.args.get('departure_time'))
+    departure_time = datetime.utcfromtimestamp(departure_time)
+    if 0 < departure_time.hour < 6:
+        model_init_date = departure_time - timedelta(days=1)
+    else:
+        model_init_date = departure_time
+    model_init_hour = 0 if 6 < departure_time.hour < 18 else 12
+    model_init_date = model_init_date.replace(hour=model_init_hour, minute=0, second=0)
     divisions = 1 if duration == 0 else duration 
     divided = np.array_split(polyline_arr, divisions)    
     init_hour = model_init_date.strftime('t%Hz')
     init_date = model_init_date.strftime('%Y%m%d')
     prog_hour = round((departure_time - model_init_date).seconds / 3600.)
-    nam_grb_req = "filter_nam_conusnest.pl?file=nam."+init_hour+".conusnest.hiresf.tm00.grib2&lev_2_m_above_ground=on&lev_surface=on&var_APCP=on&var_TMP=on&subregion=&leftlon=&rightlon=&toplat=&bottomlat=&dir=%2Fnam."+init_date
+    nam_grb_req = "filter_nam_conusnest.pl?file=nam."+init_hour+".conusnest.hiresf.tm00.grib2&lev_2_m_above_ground=on&lev_surface=on&var_APCP=on&&var_CPOFP=on&var_TMP=on&subregion=&leftlon=&rightlon=&toplat=&bottomlat=&dir=%2Fnam."+init_date
     start_lat = divided[0][0][0]
     start_lon = divided[0][0][1]
     polylines = []
@@ -125,15 +123,20 @@ def pline():
         temp = grbs.select(name='2 metre temperature')[0]
         temp.values = temp.values
         precip = grbs.select(name='Total Precipitation')[0]
+        frozen_precip = grbs.select(name='Percent frozen precipitation')[0]
         lats, lons = temp.latlons()
     #    last_point = chunk[-1]
     #    print last_point
         for point in chunk:
-            gridpoint_temp, gridpoint_precip = nearest_gridpoint(lats, lons, point[0], point[1], (temp.values, precip.values))
-            hazard_level = weather_check(gridpoint_temp, gridpoint_precip)
+            gridpoint_temp, gridpoint_precip, gridpoint_frozen_precip = nearest_gridpoint(lats, lons, point[0], point[1], (temp.values, precip.values, frozen_precip.values))
+            hazard_level, human_friendly_temp, human_friendly_precip = weather_check(gridpoint_temp, gridpoint_precip)
             pline = {}
             pline['hazard_level'] = hazard_level
             pline['coords'] = [previous_point, {'lat':point[0], 'lng':point[1]}]
+            if hazard_level in ['yellow', 'red']:
+                pline['temp'] = round((human_friendly_temp * (9/5)) + 32)
+                pline['precip'] = round(human_friendly_precip, 2)
+                pline['frozen_precip'] = gridpoint_frozen_precip
             polylines.append(pline)
             previous_point = {'lat':point[0], 'lng':point[1]}
             #print hazard_level
